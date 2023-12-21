b7f5b9977d192d18a4e68c401ea563490b33a142
chiranjib <chiranjibnath21313@gmail.com>
2023-12-21 20:18:33 +0530

    static files error fixed

--- queryxchange/settings.py
+++ queryxchange/settings.py
@@ -139,6 +139,7 @@ STATICFILES_DIRS=[
 STATIC_URL = '/static/'
 
 if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
